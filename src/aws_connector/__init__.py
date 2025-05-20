38afb60124ecf090f240ee5eef4d2e2c5bfcd6a4
Jared M. Felice <jmfel@fisherautoparts.com>
2025-05-20 12:18:54 -0400

    updated the correct init

--- src/aws_connector/__init__.py
+++ src/aws_connector/__init__.py
@@ -1,5 +1,33 @@
-"""Top-level package for aws_connector."""
+"""Top-level package for aws_connector.
+
+This package provides connectors for various AWS services including Redshift, S3, and AWS SSO.
+"""
 
 __author__ = """Jared Felice"""
 __email__ = 'jmfelice@icloud.com'
 __version__ = '0.1.0'
+
+from .redshift import RedConn, RedshiftConfig
+from .s3 import S3Connector
+from .aws_sso import AWSSSO
+from .exceptions import (
+    AWSConnectorError,
+    RedshiftError,
+    ConnectionError,
+    QueryError,
+    S3Error,
+    SSOError
+)
+
+__all__ = [
+    'RedConn',
+    'RedshiftConfig',
+    'S3Connector',
+    'AWSSSO',
+    'AWSConnectorError',
+    'RedshiftError',
+    'ConnectionError',
+    'QueryError',
+    'S3Error',
+    'SSOError'
+]
